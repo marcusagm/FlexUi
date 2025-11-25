d53bddda53d716d1c020aa272e7f4e311e553963
Marcus Maia <marcusagmaia@gmail.com>
2025-11-25 15:23:45 -0300

    fix: flexui.js

---
+++ src/FlexUI.js
@@ -0,0 +1,20 @@
+/**
+ * Description:
+ * The main entry point for the FlexUI library.
+ * It acts as a facade for the entire system, exporting only the safe,
+ * public-facing classes and APIs intended for external consumption.
+ *
+ * Usage:
+ * import { FlexUI, PanelApi } from './FlexUI.js';
+ *
+ * Exports:
+ * - FlexUI (alias for the App singleton/class)
+ * - PanelApi (Facade for Panel interaction)
+ * - GroupApi (Facade for PanelGroup interaction)
+ * - WindowApi (Facade for ApplicationWindow interaction)
+ */
+
+export { App as FlexUI } from './App.js';
+export { PanelApi } from './api/PanelApi.js';
+export { GroupApi } from './api/GroupApi.js';
+export { WindowApi } from './api/WindowApi.js';
